60787131af82654ccf580937b1efaa3734e603d6
David Hulme <dhulme@outlook.com>
2019-10-02 11:08:56 +0100

    Formatting

--- deploy.js
+++ deploy.js
@@ -1,14 +1,14 @@
 const exec = require('child_process').exec;
 
 async function execPromise(command, options) {
-	return new Promise((resolve, reject) => {
-		exec(command, options, (error, stdout, stderr) => {
-			if (error) {
-				reject(stdout);
-			}
-			resolve();
-		});
-	});
+  return new Promise((resolve, reject) => {
+    exec(command, options, (error, stdout, stderr) => {
+      if (error) {
+        reject(stdout);
+      }
+      resolve();
+    });
+  });
 }
 
 (async () => {
@@ -16,9 +16,12 @@ async function execPromise(command, options) {
   console.log('Building...');
   await execPromise('npm run build');
   console.log('Preparing deployment...');
-	await execPromise('git init', { cwd });
-	await execPromise('git add -A', { cwd });
+  await execPromise('git init', { cwd });
+  await execPromise('git add -A', { cwd });
   await execPromise('git commit -m "Deploy"', { cwd });
   console.log('Deploying...');
-	await execPromise('git push -f https://github.com/dhulme/dhulme.github.io.git master', { cwd });
+  await execPromise(
+    'git push -f https://github.com/dhulme/dhulme.github.io.git master',
+    { cwd }
+  );
 })();
