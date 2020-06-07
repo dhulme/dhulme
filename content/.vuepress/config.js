9f7f70aeb23dce521f88d63bc998eb53e93d3e97
David Hulme <dhulme@outlook.com>
2020-06-07 13:27:20 +0100

    Sitemap fix

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -1,6 +1,9 @@
+const staticFolders = ['multitrack-player', 'cv', 'spotify-stats'];
+
 module.exports = {
   title: 'David Hulme',
-  description: 'Read my blog and CV and check out the projects I\'m currently working on',
+  description:
+    "Read my blog and CV and check out the projects I'm currently working on",
   head: [
     ['link', { rel: 'icon', href: '/icon.png' }],
     [
@@ -30,21 +33,26 @@ module.exports = {
       { text: 'Projects', link: '/projects/' },
       { text: 'CV', link: 'https://dhulme.uk/cv/' }
     ]
-	},
-	plugins: [
+  },
+  plugins: [
     [
       'vuepress-plugin-clean-urls',
       {
         normalSuffix: '/',
         indexSuffix: '/',
-        notFoundPath: '/404.html',
-      },
-		],
-		[
-			'sitemap', {
-				hostname: 'https://dhulme.uk',
-				outFile: 'other/sitemap.xml'
-			},
-		]
-  ],
+        notFoundPath: '/404.html'
+      }
+    ],
+    [
+      'sitemap',
+      {
+        hostname: 'https://dhulme.uk',
+        outFile: 'other/sitemap.xml',
+        urls: staticFolders.map(url => ({
+          url: '/' + url,
+          changefreq: 'daily'
+        }))
+      }
+    ]
+  ]
 };
