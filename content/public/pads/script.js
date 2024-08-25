13cea6174b7bc0cb3edebdc4293a9071031282a0
David Hulme <dhulme@outlook.com>
2024-08-25 20:48:37 +0100

    Bass

--- content/public/pads/script.js
+++ content/public/pads/script.js
@@ -1,84 +1,127 @@
+function getHpfFrequency(value) {
+  const minValue = 20;
+  const maxValue = 20000;
+  return (
+    Math.exp((1 - value) * Math.log(maxValue / minValue)) *
+    minValue
+  );
+}
+
+function getLpfFrequency(value) {
+  const minValue = 20;
+  const maxValue = 20000;
+  return (
+    Math.exp((1 - value) * Math.log(maxValue / minValue)) *
+    minValue
+  );
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const audioElement = document.getElementById('audio');
   const padTypeSelect = document.getElementById('padTypeSelect');
   const padKeySelect = document.getElementById('padKeySelect');
-  const hpfControl = document.getElementById('hpfControl');
-  const lpfControl = document.getElementById('lpfControl');
-  const volumeControl = document.getElementById('volumeControl');
+  const bassOctaveSelect = document.getElementById('bassOctaveSelect');
+  const padHpfControl = document.getElementById('padHpfControl');
+  const padLpfControl = document.getElementById('padLpfControl');
+  const bassLpfControl = document.getElementById('bassLpfControl');
+  const padVolumeControl = document.getElementById('padVolumeControl');
   const playButton = document.getElementById('playButton');
+  const bassButton = document.getElementById('bassButton');
 
   const audioContext = new AudioContext();
 
-  const source = audioContext.createMediaElementSource(audioElement);
+  const masterGain = audioContext.createGain();
+  masterGain.gain.value = 0.5;
+  masterGain.connect(audioContext.destination);
 
-  const lpf = audioContext.createBiquadFilter();
-  lpf.type = 'lowpass';
-  lpf.frequency.value = getLpfFrequency();
+  const padSource = audioContext.createMediaElementSource(audioElement);
 
-  const hpf = audioContext.createBiquadFilter();
-  hpf.type = 'highpass';
-  hpf.frequency.value = getHpfFrequency();
+  const padLpf = audioContext.createBiquadFilter();
+  padLpf.type = 'lowpass';
+  padLpf.frequency.value = getLpfFrequency(padLpfControl.value);
 
-  const gain = audioContext.createGain();
-  gain.gain.value = getGain();
+  const padHpf = audioContext.createBiquadFilter();
+  padHpf.type = 'highpass';
+  padHpf.frequency.value = getHpfFrequency(padHpfControl.value);
 
-  function getHpfFrequency() {
-    const minValue = 20;
-    const maxValue = 20000;
-    return (
-      Math.exp((1 - hpfControl.value) * Math.log(maxValue / minValue)) *
-      minValue
-    );
+  const padGain = audioContext.createGain();
+  padGain.gain.value = 1;
+
+  const bassLpf = audioContext.createBiquadFilter();
+  bassLpf.type = 'lowpass';
+  bassLpf.frequency.value = getLpfFrequency(bassLpfControl.value);
+
+  padSource.connect(padHpf);
+  padHpf.connect(padLpf);
+  padLpf.connect(padGain);
+  padGain.connect(masterGain);
+
+  function resumeAudioContext() {
+    if (audioContext.state === 'suspended') {
+      audioContext.resume();
+    }
   }
-
-  function getLpfFrequency() {
-    const minValue = 20;
-    const maxValue = 20000;
-    return (
-      Math.exp((1 - lpfControl.value) * Math.log(maxValue / minValue)) *
-      minValue
-    );
-  }
-
-  function getGain() {
-    return 1 - volumeControl.value;
-  }
-
-  source.connect(hpf);
-  hpf.connect(lpf);
-  lpf.connect(gain);
-  gain.connect(audioContext.destination);
 
   let playing = false;
   playButton.addEventListener('click', () => {
     const audioSrc = padTypeSelect.value + '/' + padKeySelect.value;
     if (!audioElement.src.includes(audioSrc)) {
       audioElement.src = audioSrc;
-      if (audioContext.state === 'suspended') {
-        audioContext.resume();
-      }
+      resumeAudioContext();
+      playButton.classList.add('selected');
       audioElement.play();
       playing = true;
     } else {
       if (playing) {
+        playButton.classList.remove('selected');
         audioElement.pause();
       } else {
+        playButton.classList.add('selected');
         audioElement.play();
       }
       playing = !playing;
     }
   });
 
-  hpfControl.addEventListener('input', () => {
-    hpf.frequency.value = getHpfFrequency();
+  let bassEnabled = false;
+  bassButton.addEventListener('click', async () => {
+    bassEnabled = !bassEnabled;
+    resumeAudioContext();
+
+    if (bassEnabled) {
+      bassButton.classList.add('selected');
+    } else {
+      bassButton.classList.remove('selected');
+    }
+
+    const midi = await navigator.requestMIDIAccess();
+    midi.inputs.forEach((input) => {
+      input.onmidimessage = (message) => {
+        const [command, note, velocity] = message.data;
+        if (!note) return;
+        if (command === 144 && velocity > 0) {
+          playNote(note);
+        } else if (command === 128 || (command === 144 && velocity === 0)) {
+          stopNote(note);
+        }
+      };
+    });
   });
 
-  lpfControl.addEventListener('input', () => {
-    lpf.frequency.value = getLpfFrequency();
+  padHpfControl.addEventListener('input', () => {
+    padHpf.frequency.value = getHpfFrequency(padHpfControl.value);
   });
 
-  volumeControl.addEventListener('input', () => {
-    gain.gain.value = getGain();
+  padLpfControl.addEventListener('input', () => {
+    padLpf.frequency.value = getLpfFrequency(padLpfControl.value);
+  });
+
+  padVolumeControl.addEventListener('input', () => {
+    padGain.gain.value = 1 - padVolumeControl.value;
+  });
+
+  bassLpfControl.addEventListener('input', () => {
+    bassLpf.frequency.value = getLpfFrequency(bassLpfControl.value);
   });
 
   audioElement.addEventListener('timeupdate', () => {
@@ -88,4 +131,67 @@ document.addEventListener('DOMContentLoaded', () => {
       audioElement.currentTime = 10;
     }
   });
+
+  function midiNoteToFrequency(note) {
+    return (
+      440 *
+      Math.pow(2, (note - 69 + (Number(bassOctaveSelect.value) - 1) * 12) / 12)
+    );
+  }
+
+  const oscillator = audioContext.createOscillator();
+  const bassGain = audioContext.createGain();
+  bassGain.gain.value = 0;
+  oscillator.type = 'sawtooth';
+
+  oscillator.connect(bassGain);
+  bassGain.connect(bassLpf);
+  bassLpf.connect(masterGain);
+
+  oscillator.start();
+
+  const releaseTime = 0.1;
+  const attackTime = 0.1;
+
+  const notes = [];
+
+  function getMaxNote() {
+    return 60 - (Number(bassOctaveSelect.value)) * 12
+  }
+
+  function updateOscillatorFrequency() {
+    const lowestNote = Math.min(...notes);
+    if (lowestNote < getMaxNote()) {
+      oscillator.frequency.setValueAtTime(
+        midiNoteToFrequency(lowestNote),
+        audioContext.currentTime
+      );
+    }
+  }
+
+  function playNote(note) {
+    if (!bassEnabled) return;
+
+    notes.push(note);
+    if (note >= getMaxNote()) return;
+
+    const currentTime = audioContext.currentTime;
+    bassGain.gain.cancelScheduledValues(currentTime);
+    bassGain.gain.setValueAtTime(bassGain.gain.value, currentTime);
+    bassGain.gain.linearRampToValueAtTime(1, currentTime + attackTime);
+    updateOscillatorFrequency();
+  }
+
+  function stopNote(note) {
+    notes.splice(notes.indexOf(note), 1);
+    const currentTime = audioContext.currentTime;
+
+    if (notes.length === 0) {
+      bassGain.gain.cancelScheduledValues(currentTime);
+      bassGain.gain.setValueAtTime(bassGain.gain.value, currentTime);
+      bassGain.gain.linearRampToValueAtTime(0, currentTime + releaseTime);
+    } else {
+      updateOscillatorFrequency();
+    }
+  }
 });
